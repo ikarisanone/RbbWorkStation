63f6fb553da1aee2e74bfe7e4bb8bcd3f7d28005
Korhan Erem <korhanerem@gmail.com>
2025-07-26 20:42:22 +0300

    Last

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://avygqkwcovsftojbljsc.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2eWdxa3djb3ZzZnRvamJsanNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODczNTQsImV4cCI6MjAzMjQ2MzM1NH0.QdD_N5t55eTieE6tr63g9n830GzMFFZjpJInl65S0Z4'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
